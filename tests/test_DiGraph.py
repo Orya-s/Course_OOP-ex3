4a5d7f894143d173c9e63b1ee601d06bda13d6e0
orya.sp@gmail.com <61458890+Orya-s@users.noreply.github.com>
2020-12-30 19:56:55 +0200

    remove edge

---
+++ tests/test_DiGraph.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+
+class TestNodeData(TestCase):
+    def test_get_ni(self):
+        self.fail()
+
+    def test_get_parents(self):
+        self.fail()
+
+    def test_has_ni(self):
+        self.fail()
+
+    def test_has_parent(self):
+        self.fail()
+
+
+class TestDiGraph(TestCase):
+    def test_get_node(self):
+        self.fail()
+
+    def test_v_size(self):
+        self.fail()
+
+    def test_e_size(self):
+        self.fail()
+
+    def test_get_mc(self):
+        self.fail()
+
+    def test_add_edge(self):
+        self.fail()
+
+    def test_add_node(self):
+        self.fail()
+
+    def test_remove_node(self):
+        self.fail()
+
+    def test_remove_edge(self):
+        self.fail()
